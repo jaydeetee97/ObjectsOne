6015a5fca9cd4884a59170696810a0de5ff72fe2
jaydeetee97 <162945338+jaydeetee97@users.noreply.github.com>
2024-06-25 15:44:54 -0400

    Update Person.java

--- object1/src/main/java/object1/Person.java
+++ object1/src/main/java/object1/Person.java
@@ -1,1 +1,21 @@
+// Define a class
+class Person {
+    // Define Instance Variables
+    // different for each object!
+    String name;
+    int age = 0;
 
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        // Declare and initialize an object
+        Person person1 = new Person("John", 30);
+
+    /* Now you can use the Object `person1` */
+        
+    }
+}
