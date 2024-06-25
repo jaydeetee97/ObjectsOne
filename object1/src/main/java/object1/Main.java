5e5b94b33e76060d67c7f6613d987ebc9b48be83
jaydeetee97 <162945338+jaydeetee97@users.noreply.github.com>
2024-06-25 16:12:32 -0400

    finished

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -5,8 +5,8 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String... args) {
-        Person person2 = new Person()
-        System.out.println(person2.height);
+        Person newPerson = new Person()
+        System.out.println(newPerson.height);
         Logger logger=Logger.getLogger(Main.class.getName());
         logger.info("This is a module-using Hello World!");    }
 }
