1e376d1e62db1928f5e3ef2e6f824b88865b2ee5
jaydeetee97 <162945338+jaydeetee97@users.noreply.github.com>
2024-06-25 15:51:22 -0400

    Update Main.java

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -5,6 +5,8 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String... args) {
+        Main Person = new Main();
+        System.out.println(Person.height);
         Logger logger=Logger.getLogger(Main.class.getName());
         logger.info("This is a module-using Hello World!");    }
 }
